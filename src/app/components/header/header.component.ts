c8b29fc2522dbb2729fc860ce90636c51d0777c0
ductran17 <mrductran173@gmail.com>
2025-01-17 17:24:18 +0700

    Add button to header, fix navigation in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  imports: [CommonModule, RouterModule]
 })
 export class HeaderComponent {}
